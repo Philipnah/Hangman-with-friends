ffa73e32609a7a1c32eae95bb86913ce62bcdb1d
Philip Han <46556419+Philipnah@users.noreply.github.com>
2023-07-20 14:25:04 +0200

    Add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
@@ -9,16 +10,19 @@ import Game from "./pages/Game.jsx";
 
 function App() {
 	return (
-		<Router>
-			<h1 className="text-white text-center text-4xl font-black mt-16 mb-6">Hangman with Friends!</h1>
-			<Navbar />
-			<Routes>
-				<Route exact path="/" Component={CreateGame} />
-				<Route path="/joingame" Component={JoinGame} />
-				<Route path="/game/:code" Component={Game} />
-			</Routes>
-			<Footer />
-		</Router>
+		<>
+			<Router>
+				<h1 className="text-white text-center text-4xl font-black mt-16 mb-6">Hangman with Friends!</h1>
+				<Navbar />
+				<Routes>
+					<Route exact path="/" Component={CreateGame} />
+					<Route path="/joingame" Component={JoinGame} />
+					<Route path="/game/:code" Component={Game} />
+				</Routes>
+				<Footer />
+			</Router>
+			<Analytics />
+		</>
 	);
 }
 
